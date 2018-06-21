2621d427126d7ad12cb32c3df0512c69a8fdcae4
Ross Hartley <m.ross.hartley@gmail.com>
2018-06-21 16:42:57 -0400

    changed name of twp step c_Frost example

---
+++ Optimizations/SingleSupport/TwoStep_SS/default_c_frost/+TwoStep_SS_default_c_frost/+utils/getBounds.m
@@ -0,0 +1,155 @@
+function bounds = getBounds(robot)
+
+% Get Bounds
+model_bounds = robot.getLimits();
+bounds = struct();
+
+model_bounds.states.x.lb(1:3) = [-10,-10,-10];
+model_bounds.states.x.ub(1:3) = [10,10,10];
+
+% model_bounds.states.x.lb(4:6) = deg2rad(-15);
+% model_bounds.states.x.ub(4:6) = deg2rad(15);
+
+% model_bounds.states.x.lb([7,8,14,15]) = deg2rad(-15);
+% model_bounds.states.x.ub([7,8,14,15]) = deg2rad(15);
+
+model_bounds.inputs.Control.u.lb([5,10]) = -0.01;
+model_bounds.inputs.Control.u.ub([5,10]) = 0.01;
+
+model_bounds.average_pitch.lb = deg2rad(0);
+model_bounds.average_pitch.ub = deg2rad(0);
+% 
+% model_bounds.average_yaw.lb = deg2rad(0);
+% model_bounds.average_yaw.ub = deg2rad(0);
+% 
+% model_bounds.average_hip_abduction.lb = deg2rad(0);
+% model_bounds.average_hip_abduction.ub = deg2rad(0);
+% 
+model_bounds.average_hip_rotation.lb = deg2rad(0);
+model_bounds.average_hip_rotation.ub = deg2rad(0);
+
+model_bounds.average_velocity.lb = 0.0;
+model_bounds.average_velocity.ub = 0.0;
+
+model_bounds.foot_clearance.lb = 0.15;
+model_bounds.foot_clearance.ub = 0.155;
+
+model_bounds.distance_pelvis_to_stance_toe.lb = 0.5;
+model_bounds.distance_pelvis_to_stance_toe.ub = 1.0;
+
+model_bounds.toe_to_toe_width.lb = -0.40; % 0.27 nominal width
+model_bounds.toe_to_toe_width.ub = -0.10;
+
+%% Right Stance
+bounds.RightStance = model_bounds;
+
+bounds.RightStance.time.t0.lb = 0;
+bounds.RightStance.time.t0.ub = 0;
+bounds.RightStance.time.t0.x0 = 0;
+
+bounds.RightStance.time.tf.lb = 0.4;
+bounds.RightStance.time.tf.ub = 0.4;
+bounds.RightStance.time.tf.x0 = 0.4;
+
+bounds.RightStance.time.duration.lb = 0.4;
+bounds.RightStance.time.duration.ub = 0.4;
+bounds.RightStance.time.duration.x0 = 0.4;
+
+bounds.RightStance.inputs.ConstraintWrench.fRightToeBottom.lb = -10000;
+bounds.RightStance.inputs.ConstraintWrench.fRightToeBottom.ub = 10000;
+bounds.RightStance.inputs.ConstraintWrench.fRightToeBottom.x0 = 100;
+
+bounds.RightStance.inputs.ConstraintWrench.ffixedKneeSpring.lb = -10000;
+bounds.RightStance.inputs.ConstraintWrench.ffixedKneeSpring.ub = 10000;
+bounds.RightStance.inputs.ConstraintWrench.ffixedKneeSpring.x0 = 100;
+
+bounds.RightStance.inputs.ConstraintWrench.ffourBar.lb = -10000;
+bounds.RightStance.inputs.ConstraintWrench.ffourBar.ub = 10000;
+bounds.RightStance.inputs.ConstraintWrench.ffourBar.x0 = 100;
+
+bounds.RightStance.params.pfixedKneeSpring.lb = -0*ones(2,1);
+bounds.RightStance.params.pfixedKneeSpring.ub = 0*ones(2,1);
+bounds.RightStance.params.pfixedKneeSpring.x0 = zeros(2,1);
+
+bounds.RightStance.params.pfourBar.lb = -0*ones(2,1);
+bounds.RightStance.params.pfourBar.ub = 0*ones(2,1);
+bounds.RightStance.params.pfourBar.x0 = zeros(2,1);
+
+bounds.RightStance.params.pRightToeBottom.lb = [0;0;0;0;-pi/2];
+bounds.RightStance.params.pRightToeBottom.ub = [0;0;0;0;-pi/2];
+bounds.RightStance.params.pRightToeBottom.x0 = [0;0;0;0;-pi/2];
+
+bounds.RightStance.params.atime.lb = -10*ones(6*10,1);
+bounds.RightStance.params.atime.ub = 10*ones(6*10,1);
+bounds.RightStance.params.atime.x0 = zeros(6*10,1);
+
+bounds.RightStance.params.ptime.lb = [bounds.RightStance.time.tf.lb, bounds.RightStance.time.t0.lb];
+bounds.RightStance.params.ptime.ub = [bounds.RightStance.time.tf.ub, bounds.RightStance.time.t0.ub];
+bounds.RightStance.params.ptime.x0 = [bounds.RightStance.time.t0.x0, bounds.RightStance.time.tf.x0];
+
+bounds.RightStance.time.kp = 100;
+bounds.RightStance.time.kd = 20;
+
+
+
+%% Left Stance
+bounds.LeftStance = model_bounds;
+
+bounds.LeftStance.time.t0.lb = 0;
+bounds.LeftStance.time.t0.ub = 0;
+bounds.LeftStance.time.t0.x0 = 0;
+
+bounds.LeftStance.time.tf.lb = 0.4;
+bounds.LeftStance.time.tf.ub = 0.4;
+bounds.LeftStance.time.tf.x0 = 0.4;
+
+bounds.LeftStance.time.duration.lb = 0.4;
+bounds.LeftStance.time.duration.ub = 0.4;
+bounds.LeftStance.time.duration.x0 = 0.4;
+
+bounds.LeftStance.inputs.ConstraintWrench.fLeftToeBottom.lb = -10000;
+bounds.LeftStance.inputs.ConstraintWrench.fLeftToeBottom.ub = 10000;
+bounds.LeftStance.inputs.ConstraintWrench.fLeftToeBottom.x0 = 100;
+
+bounds.LeftStance.inputs.ConstraintWrench.ffixedKneeSpring.lb = -10000;
+bounds.LeftStance.inputs.ConstraintWrench.ffixedKneeSpring.ub = 10000;
+bounds.LeftStance.inputs.ConstraintWrench.ffixedKneeSpring.x0 = 100;
+
+bounds.LeftStance.inputs.ConstraintWrench.ffourBar.lb = -10000;
+bounds.LeftStance.inputs.ConstraintWrench.ffourBar.ub = 10000;
+bounds.LeftStance.inputs.ConstraintWrench.ffourBar.x0 = 100;
+
+bounds.LeftStance.params.pfixedKneeSpring.lb = -0*ones(2,1);
+bounds.LeftStance.params.pfixedKneeSpring.ub = 0*ones(2,1);
+bounds.LeftStance.params.pfixedKneeSpring.x0 = zeros(2,1);
+
+bounds.LeftStance.params.pfourBar.lb = -0*ones(2,1);
+bounds.LeftStance.params.pfourBar.ub = 0*ones(2,1);
+bounds.LeftStance.params.pfourBar.x0 = zeros(2,1);
+
+bounds.LeftStance.params.pLeftToeBottom.lb = [-10;-10;-10;0;-pi/2];
+bounds.LeftStance.params.pLeftToeBottom.ub = [10;10;10;0;-pi/2];
+bounds.LeftStance.params.pLeftToeBottom.x0 = [0;0;0;0;-pi/2];
+
+bounds.LeftStance.params.atime.lb = -10*ones(6*10,1);
+bounds.LeftStance.params.atime.ub = 10*ones(6*10,1);
+bounds.LeftStance.params.atime.x0 = zeros(6*10,1);
+
+bounds.LeftStance.params.ptime.lb = [bounds.LeftStance.time.tf.lb, bounds.LeftStance.time.t0.lb];
+bounds.LeftStance.params.ptime.ub = [bounds.LeftStance.time.tf.ub, bounds.LeftStance.time.t0.ub];
+bounds.LeftStance.params.ptime.x0 = [bounds.LeftStance.time.t0.x0, bounds.LeftStance.time.tf.x0];
+
+bounds.LeftStance.time.kp = 100;
+bounds.LeftStance.time.kd = 20;
+
+bounds.LeftStance.toe_to_toe_width.lb = -model_bounds.toe_to_toe_width.ub; 
+bounds.LeftStance.toe_to_toe_width.ub = -model_bounds.toe_to_toe_width.lb;
+
+%% Impacts
+
+% Right Impact
+bounds.RightImpact = model_bounds;
+% Left Impact
+bounds.LeftImpact = model_bounds;
+
+end
---
+++ Optimizations/SingleSupport/TwoStep_SS/default_c_frost/cassie_opt.m
@@ -0,0 +1,194 @@
+%% Setup
+restoredefaultpath; matlabrc; clear; clc;
+root = fileparts(fileparts(fileparts(fileparts(pwd))));
+addpath(genpath(root));
+PATHS = cassie.utils.getPaths(root);
+frost_addpath;
+
+addpath('C:\Users\Ross Hartley\Documents\GitHub\Ross_Cassie_Controllers\submodules\C-Frost\Matlab')
+
+% Settings
+LOAD = false;
+COMPILE = false;
+SAVE = false;
+
+% Load hybrid system
+robot = Cassie([PATHS.MODEL,'\urdf\cassie_with_sensors.urdf']);
+if LOAD
+    robot.loadDynamics(PATHS.MODEL_LOAD, true);
+    [sys, domains, guards] = cassie.load_behavior(robot, PATHS.OPT_LOAD);
+else
+    robot.configureDynamics('DelayCoriolisSet',true,'OmitCoriolisSet',true);
+    [sys, domains, guards] = cassie.load_behavior(robot, '');
+end
+    
+
+%% Create optimization problem
+num_grid.RightStance = 10;
+num_grid.LeftStance = 10;
+nlp = HybridTrajectoryOptimization('Cassie_TwoStep_SS',sys,num_grid,[],'EqualityConstraintBoundary',1e-4);
+nlp.update;
+
+% Add domain specific constraints
+nlp.Phase(1).Plant.UserNlpConstraint = @TwoStep_SS_default_c_frost.opt.right_stance;
+nlp.Phase(2).Plant.UserNlpConstraint = @TwoStep_SS_default_c_frost.opt.left_impact;
+nlp.Phase(3).Plant.UserNlpConstraint = @TwoStep_SS_default_c_frost.opt.left_stance;
+nlp.Phase(4).Plant.UserNlpConstraint = @TwoStep_SS_default_c_frost.opt.right_impact;
+nlp.update;
+
+% Configure bounds and update
+bounds = TwoStep_SS_default_c_frost.utils.getBounds(robot);
+if LOAD
+    nlp.configure(bounds, PATHS.OPT_LOAD);
+else
+    nlp.configure(bounds);
+end
+
+% Add Multi-domain constraints
+% nlp = TwoStep_SS_default_c_frost.opt.multi_domain_constraints(nlp);
+% nlp.update;
+
+
+%% Compile and Save
+if COMPILE
+    compileObjective(nlp,[],[],PATHS.OPT_EXPORT);
+%     compileConstraint(nlp,[],[],PATHS.OPT_EXPORT);
+    compileConstraint(nlp,[],[],PATHS.OPT_EXPORT,'dynamics_equation');
+%     nlp.Phase(3).ConstrTable.dynamics_equation(1).SummandFunctions(end).SymFun.export(PATHS.OPT_EXPORT, 'ForceExport',true)
+%     nlp.Phase(3).ConstrTable.dynamics_equation(1).SummandFunctions(end).SymFun.exportJacobian(PATHS.OPT_EXPORT, 'ForceExport',true)
+end
+
+% Save
+if SAVE
+    sys.saveExpression(PATHS.OPT_LOAD);
+end
+
+% Compile Single Constraint
+% compileConstraint(nlp.Phase(1),'swingFootClearance_RightStance',PATHS.OPT_EXPORT);
+
+% Remove Single Constraint
+% removeConstraint(nlp.Phase(1),'dynamics_equation');
+% removeConstraint(nlp.Phase(3),'dynamics_equation');
+% removeConstraint(nlp.Phase(1),'BezierSymmetry');
+
+
+%% Solve Optimization Problem 
+
+% -------- Velocity ---------
+velocity = [1;0.3];
+nlp.Phase(1).updateConstrProp('average_velocity_RightStance', 'last', 'lb', velocity, 'ub', velocity);
+nlp.Phase(3).updateConstrProp('average_velocity_LeftStance', 'last', 'lb', velocity, 'ub', velocity);
+
+% ------- Step Height ---------
+step_height = 0.2;
+% Right Stance Guard
+for k = 1:length(nlp.Phase(1).ConstrTable.u_leftToeHeight_cassie)-1
+    nlp.Phase(1).ConstrTable.u_leftToeHeight_cassie(k).updateProp('lb', -Inf, 'ub', Inf);
+end
+nlp.Phase(1).ConstrTable.u_leftToeHeight_cassie(end).updateProp('lb', step_height, 'ub', step_height);
+% Left Stance Guard
+for k = 1:length(nlp.Phase(3).ConstrTable.u_rightToeHeight_cassie)-1
+    nlp.Phase(3).ConstrTable.u_rightToeHeight_cassie(k).updateProp('lb', -Inf, 'ub', Inf);
+end
+nlp.Phase(3).ConstrTable.u_rightToeHeight_cassie(end).updateProp('lb', 2*step_height, 'ub', 2*step_height);
+% ----------------------------
+
+
+
+%% Create Optimization Problem 
+
+% Create Ipopt solver
+ipopt_options.max_iter         = 3000;
+ipopt_options.tol              = 1e-2;
+ipopt_options.dual_inf_tol           = 0e0;
+ipopt_options.constr_viol_tol        = 1e-4;
+ipopt_options.compl_inf_tol          = 1e0;
+    
+solver = IpoptApplication(nlp, ipopt_options);
+% solver = IpoptApplication(nlp);
+old = load('x0');
+
+
+%% Create c-frost problem
+GENERATE_C = true;
+GENERATE_C_COMPILE = true;
+ROOT_PATH = 'periodic';
+c_code_path = fullfile(ROOT_PATH, 'c_code');
+src_path = fullfile(ROOT_PATH, 'c_code', 'src');
+src_gen_path = fullfile(ROOT_PATH, 'c_code', 'src', 'gen');
+include_dir = fullfile(ROOT_PATH, 'c_code', 'include');
+data_path = fullfile(ROOT_PATH, 'c_code', 'res');
+local_path = fullfile(ROOT_PATH, 'local');
+local_res_path = fullfile(ROOT_PATH, 'local', 'res');
+local_output_path = fullfile(ROOT_PATH, 'local', 'output');
+local_log_path = fullfile(ROOT_PATH, 'local', 'log');
+
+if GENERATE_C
+    if ~exist(c_code_path, 'dir')
+        mkdir(c_code_path);
+    end
+    if ~exist(src_path, 'dir')
+        mkdir(src_path);
+    end
+    if ~exist(src_gen_path, 'dir')
+        mkdir(src_gen_path);
+    end
+    if ~exist(include_dir, 'dir')
+        mkdir(include_dir);
+    end
+    if ~exist(data_path, 'dir')
+        mkdir(data_path);
+    end
+    if ~exist(local_path, 'dir')
+        mkdir(local_path);
+    end
+    if ~exist(local_res_path, 'dir')
+        mkdir(local_res_path);
+    end
+    if ~exist(local_output_path, 'dir')
+        mkdir(local_output_path);
+    end
+    if ~exist(local_log_path, 'dir')
+        mkdir(local_log_path);
+    end
+    
+    if GENERATE_C_COMPILE
+        [funcs, map] = frost_c.getAllFuncs(solver);
+        frost_c.createFunctionListHeader(funcs, src_path, include_dir);
+        frost_c.createIncludeHeader(funcs, include_dir);
+%         frost_c.createConstraints(nlp,[],[],src_gen_path, include_dir,{'dynamics_equation','dxDiscreteMapLeftImpact','dxDiscreteMapRightImpact'})
+%         frost_c.createConstraints(nlp,[],[],src_gen_path, include_dir)
+%         frost_c.createObjectives(nlp,[],[],src_gen_path, include_dir);
+        save(fullfile(local_res_path, 'funcs'), 'funcs');
+    end
+    load(fullfile(local_res_path, 'funcs'))
+    frost_c.createDataFile(solver, funcs, data_path, 'data');
+    frost_c.createBoundsFile(solver, funcs, data_path, 'bounds');
+    frost_c.createInitialGuess(solver, data_path, old.solution.x);
+end
+
+% Example commands
+% frost_c.createConstraints(nlp, 11, 'average_velocity_RightStance', src_gen_path, include_dir,'dynamics_equation')
+% frost_c.createConstraints(nlp, 11, 'StartingStatedX_RightStanceDown1', src_gen_path, include_dir,'dynamics_equation')
+% ./program --initial 'res/init.json' --options '../ipopt.opt' --data 'res/data.json' --bounds 'res/bounds.json' --output '../local/output/output.json'
+
+
+%% Load c_frost results
+sol = loadjson('periodic/local/output/output.json');
+
+%% Extract optimization results
+[tspan, states, inputs, params] = exportSolution(nlp, sol);
+solution.x = sol;
+solution.tspan = tspan;
+solution.states = states;
+solution.inputs = inputs;
+solution.params = params;
+ 
+% checkConstraints(nlp, sol, 1e-4, 'log.txt')
+
+%% Animate 
+q_log = [solution.states{1}.x, solution.states{3}.x]; 
+t_log = [solution.tspan{1}, solution.tspan{1}(end) + solution.tspan{3}];
+conGUI = cassie.load_animation(robot, t_log, q_log)';
+
+
